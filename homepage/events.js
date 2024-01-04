be668e3f8c547894eb44efc5bb839d9c7942d8dd
devbatsy <153440894+devbatsy@users.noreply.github.com>
2024-01-04 04:21:08 -0800

    Add files via upload

---
+++ homepage/events.js
@@ -0,0 +1,32 @@
+const searchPri = document.querySelectorAll('.searchBar');
+const searchpSec = document.querySelector('.auxSearchCont');
+const secInput = searchpSec.querySelector('input');
+const subTwo = document.querySelector('#two');
+
+searchPri.forEach(val =>{
+    val.addEventListener('click', e =>{
+        searchpSec.style.display = 'flex';
+        secInput.focus();
+    })
+})
+
+secInput.addEventListener('focusout', e =>{
+    searchpSec.style.display = 'none';
+})
+
+addEventListener('resize', e =>{
+    setParameters()
+})
+addEventListener('load', e =>{
+    setParameters()
+})
+function setParameters()
+{
+    const subCont = document.querySelectorAll('.subContainer')
+    let length = window.getComputedStyle(subCont[0]).height
+    length = Number(length.slice(0,length.length-2));
+    document.querySelector('.scrollCustom').style.top = `${(length-60)/2}px`;
+    bars.forEach(val =>{
+        val.style.height = `${length-50}px`;
+    })
+}
